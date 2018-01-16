d3d2730c8c5d497b6ab27f45f5ed037d6e5807e8
SALOMON Kévin <ksalomon09120@gmail.com>
2018-01-16 11:34:15 +0100

    Save 1

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index.ejs');
+    res.render('login.ejs');
 });
 
 module.exports = router;
