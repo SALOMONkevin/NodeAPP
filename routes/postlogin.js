1e1fa1191efa5d054fd19561a5ae98151bfeec42
Arthur <deguinesarthur@gmail.com>
2018-01-17 14:39:20 +0100

    bouton enregister

--- routes/postlogin.js
+++ routes/postlogin.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.post('/', function (req, res) {
     const db = require('./dataBase');
     console.log(req.body.register_name);
-    alert(db.connection("select * from utilisateur"));
+    db.connection("select * from utilisateur");
     res.render('test.ejs');
 });
 
