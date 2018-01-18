2c5e755a46bf13925461cec993dab28b9146807f
SALOMON Kévin <ksalomon09120@gmail.com>
2018-01-18 14:39:36 +0100

    sef

--- public/js/formulairejslog.js
+++ public/js/formulairejslog.js
@@ -43,10 +43,16 @@
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).attr('type') == 'e-mail' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
+        }
+        if($(input).attr('name') == 'register_password2') {
+            if($(input).val().trim() != $("#password").val().trim()) {
+                return false;
+            }
+     
         }
         else {
             if($(input).val().trim() == ''){
